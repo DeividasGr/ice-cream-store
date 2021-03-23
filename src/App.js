06121277678ee0518e3bc55d46bcc1b85cafcd5b
Gruzinas1 <dgruzdzevicius@gmail.com>
2021-03-23 14:14:15 +0200

    Code and test for error cathing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Options from './pages/entry/Options';
 import SummaryForm from './pages/summary/SummaryForm';
 
 function App() {
   return (
     <div>
       <SummaryForm />
-      <Options optionType="toppings" />
     </div>
   );
 }
--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from 'react';
 import ScoopOptions from './ScoopOptions';
 import ToppingOptions from './ToppingOptions';
+import AlertBanner from '../../components/AlertBanner';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 
 function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemConponent = optionType === 'scoops' ? ScoopOptions : ToppingOptions;
 
